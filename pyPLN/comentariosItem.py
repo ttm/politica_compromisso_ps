c3528ee5c974ec96f420fd695b5608213fb5283a
Green Kobold <renato.fabbri@gmail.com>
2013-07-31 19:48:06 -0300

    adicionado script com os comentarios por item para comentar

---
+++ pyPLN/comentariosItem.py
@@ -0,0 +1,7 @@
+#-*- coding: utf8 -*-
+
+# este arquivo faz estatísticas simples sobre comentários e itens para comentar
+
+# compromisso, no dia 29/07, cada índice é um item, que acessa o número de comentários:
+comentarios=[3, 2, 1, 0, 0, 3,    4,    1, 0, 0, 0, 0,     1, 0, 2, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1,     1, 0, 0,     0, 1, 0, 0]
+extra=6
